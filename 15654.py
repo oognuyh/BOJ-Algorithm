293e64db64b27876fc40720b1c867f16baa3b628
oognuyh <48203569+oognuyh@users.noreply.github.com>
2020-08-26 23:27:56 +0900

    hg

---
+++ 15654.py
@@ -0,0 +1,8 @@
+"""
+    15654. N과 M (5)
+"""
+from itertools import permutations
+
+N, M = map(int, input().split())
+targets = map(str, sorted(list(map(int, input().split()))))
+print('\n'.join(map(' '.join, permutations(targets, M))))
