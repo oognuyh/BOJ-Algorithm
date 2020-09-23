ffdf6c94b89644e8a4a8ae26b73daff7a3cb47db
oognuyh <48203569+oognuyh@users.noreply.github.com>
2020-09-23 23:53:00 +0900

    hg

--- 15654.py
+++ 15654.py
@@ -5,4 +5,5 @@ from itertools import permutations
 
 N, M = map(int, input().split())
 targets = map(str, sorted(list(map(int, input().split()))))
+
 print('\n'.join(map(' '.join, permutations(targets, M))))
