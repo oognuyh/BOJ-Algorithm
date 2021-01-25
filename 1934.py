f3ddd4eee4f2b56b114832fb7ef1e35fbc730d70
Hyungoo Kang <48203569+oognuyh@users.noreply.github.com>
2021-01-25 23:50:52 +0900

    Add files via upload

---
+++ 1934.py
@@ -0,0 +1,18 @@
+"""
+    1934. 최소공배수
+"""
+from sys import stdin
+
+def run(a, b):
+    def gcd(a, b):
+        return gcd(b % a, a) if b % a else a
+
+    def lcm(a, b):
+        return a * (b // gcd(a, b))
+    
+    print(lcm(a, b))
+
+if __name__ == "__main__":
+    for _ in range(int(stdin.readline().strip())):
+        a, b = map(int, stdin.readline().strip().split())
+        run(a, b)
