eb5652b1cc86043f4cfd543711b9c1566c6a7f7b
Hyungoo Kang <48203569+oognuyh@users.noreply.github.com>
2021-03-09 00:25:47 +0900

    hg

--- 1260.py
+++ 1260.py
@@ -2,38 +2,46 @@
     1260. DFS와 BFS
 """
 from sys import stdin
+from collections import deque
 read = lambda : stdin.readline().strip()
 
 def run(N, M, V):
     def dfs(v):
-        print(v, end=' ')
-        visited[v] = True
+        print(v, end = " ")
+
+        dfs_visited[v] = True
+
         for to in range(1, N + 1):
-            if not visited[to] and graph[v][to]:
+            if not dfs_visited[to] and graph[v][to]:
                 dfs(to)
 
     def bfs(v):
-        q = [v]
-        visited[v] = False
+        print()
+
+        bfs_visited[v] = True
+        q = deque([v])
 
         while q:
-            v = q[0]
-            print(v, end=' ')
-            del q[0]
-            for i in range(1, N + 1):
-                if visited[i] == 1 and graph[v][i] == 1:
-                    q.append(i)
-                    visited[i] = 0
+            _v = q.popleft()
+            
+            print(_v, end = " ")
+            
+            for to in range(1, N + 1):
+                if not bfs_visited[to] and graph[_v][to]:
+                    q.append(to)
+                    bfs_visited[to] = True
     
-    connections = [tuple(map(int, read().split())) for _ in range(M)]
-    graph = [[True if (x, y) in connections or (y, x) in connections else False for x in range(N + 1)] for y in range(N + 1)]
+    graph = [[False for x in range(N + 1)] for y in range(N + 1)]
+    
+    for _ in range(M):
+        _from, _to = map(int, read().split())
+        graph[_from][_to], graph[_to][_from] = True, True
 
-    visited = [False for _ in range(N + 1)]
+    dfs_visited = [False for _ in range(N + 1)]
+    bfs_visited = [False for _ in range(N + 1)]
 
-    dfs(v)
-    print()
-    bfs(v)
-
+    dfs(V)
+    bfs(V)
 
 if __name__ == "__main__":
     run(*map(int, read().split()))
