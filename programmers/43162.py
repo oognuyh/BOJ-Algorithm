8855390952db06bfc783658399162a7f2058479c
Hyungoo Kang <oognuyh@gmail.com>
2021-06-07 23:50:12 +0900

    hg

---
+++ programmers/43162.py
@@ -0,0 +1,38 @@
+"""
+    43162. 네트워크
+"""
+from collections import defaultdict
+
+def solution(n, computers):
+    def connect(i, j):
+        for k in range(n):
+            if computers[i][k]:                
+                computers[i][k] = 0
+                connect(k, i)
+
+    num_of_networks = 0
+
+    for i in range(n):
+        for j in range(n):
+            if computers[i][j]:
+                connect(i, j)
+                num_of_networks += 1
+
+    return num_of_networks
+
+"""
+    # Others
+        1. 
+            def solution(n, computers):
+                temp = list(range(n))
+
+                for i in range(n):
+                    for j in range(n):
+                        if computers[i][j]:
+                            for k in range(n):
+                                if temp[k] == temp[i]:
+                                    temp[k] = temp[j]
+
+                return len(set(temp))
+"""
+
