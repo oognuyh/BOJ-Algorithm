7ecb127b1fe9c86863405f40f91c3bb51d810d46
Hyungoo Kang <oognuyh@gmail.com>
2021-08-26 23:44:20 +0900

    hg

---
+++ programmers/12899.java
@@ -0,0 +1,16 @@
+/*
+ * 12899. 124 나라의 숫자
+ */
+class Solution {
+    public String solution(int n) {
+        String[] num = {"4","1","2"};
+        String answer = "";
+  
+        while(n > 0){
+            answer = num[n % 3] + answer;
+            n = (n - 1) / 3;
+        }
+
+        return answer;
+    }
+  }
