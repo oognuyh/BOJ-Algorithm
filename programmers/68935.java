bd301fa10ffbc1163b4cf9a0dbe37e42e7a49f03
Hyungoo Kang <oognuyh@gmail.com>
2021-10-15 23:15:17 +0900

    hg

---
+++ programmers/68935.java
@@ -0,0 +1,9 @@
+/*
+ * 68935. 3진법 뒤집기
+ */
+class Solution {
+    public int solution(int n) {
+        return Integer.parseInt(new StringBuilder(Integer.toString(n, 3))
+            .reverse().toString() , 3);
+    }
+}
